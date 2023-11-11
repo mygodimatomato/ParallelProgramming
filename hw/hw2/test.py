91ee6d1ac6c8b6d21437e8b1911e3422fc720986
mygodimatomato <kullonono@gmail.com>
2023-11-12 03:24:33 +0800

    add test.py

--- hw/hw2/test.py
+++ hw/hw2/test.py
@@ -7,9 +7,9 @@ test_2 = []
 
 # use size, recvcounts, displacement to get the sorted test_2, which should be [0,1,2,3,4,5,6,7,8,9,10,11,12]
 # append test_2 with the sorted numbers
-for j in range(3):
-  for i in range(size):
-    test_2.append(test[displacements[i] + j])
+for i in range(3):
+  for j in range(size):
+    test_2.append(test[displacements[j] + i])
 for i in range(whole_len%size):
   test_2.append(test[displacements[i] + 3] )
 
