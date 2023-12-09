8a594803245b83a884b072a4086957ed39dd8755
mygodimatomato <kullonono@gmail.com>
2023-12-10 01:09:49 +0800

    lab4

--- Lab/Lab04/lab4.cu
+++ Lab/Lab04/lab4.cu
@@ -3,14 +3,28 @@
 #include <cassert>
 #include <zlib.h>
 #include <png.h>
-#include <cuda_fp16.h>
+#include <cuda_runtime.h>
 
-#define Z 2
-#define Y 5
-#define X 5
-#define xBound X / 2
-#define yBound Y / 2
+#define MASK_N 2
+#define MASK_X 5
+#define MASK_Y 5
 #define SCALE 8
+
+/* Hint 7 */
+// this variable is used by device
+// __constant__ int const_mask[MASK_N][MASK_X][MASK_Y];
+__device__ int mask[MASK_N][MASK_X][MASK_Y] = { 
+    {{ -1, -4, -6, -4, -1},
+     { -2, -8,-12, -8, -2},
+     {  0,  0,  0,  0,  0}, 
+     {  2,  8, 12,  8,  2}, 
+     {  1,  4,  6,  4,  1}},
+    {{ -1, -2,  0,  2,  1}, 
+     { -4, -8,  0,  8,  4}, 
+     { -6,-12,  0, 12,  6}, 
+     { -4, -8,  0,  8,  4}, 
+     { -1, -2,  0,  2,  1}} 
+};
 
 int read_png(const char* filename, unsigned char** image, unsigned* height, 
              unsigned* width, unsigned* channels) {
@@ -83,109 +97,96 @@ void write_png(const char* filename, png_bytep image, const unsigned height, con
     fclose(fp);
 }
 
-__constant__ char mask[Z][Y][X] = { { { -1, -4, -6, -4, -1 },
-                                        { -2, -8, -12, -8, -2 },
-                                        { 0, 0, 0, 0, 0 },
-                                        { 2, 8, 12, 8, 2 },
-                                        { 1, 4, 6, 4, 1 } },
-                                      { { -1, -2, 0, 2, 1 },
-                                        { -4, -8, 0, 8, 4 },
-                                        { -6, -12, 0, 12, 6 },
-                                        { -4, -8, 0, 8, 4 },
-                                        { -1, -2, 0, 2, 1 } } };
 
-inline __device__ int bound_check(int val, int lower, int upper) {
-    if (val >= lower && val < upper)
-        return 1;
-    else
-        return 0;
-}
-__global__ void sobel(unsigned char *s, unsigned char *t, unsigned height, unsigned width, unsigned channels) {
-    int tid = blockIdx.x * blockDim.x + threadIdx.x; // go from 0 to width - 1
-    __half val[Z][3];
-    __half zero = __float2half(0.0f);
+__global__ void sobel(unsigned char* s, unsigned char* t, unsigned height, unsigned width, unsigned channels) {
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+    int i, v, u;
+    int R, G, B;
+    int adjustX = (MASK_X % 2) ? 1 : 0;
+    int adjustY = (MASK_X % 2) ? 1 : 0;
+    int xBound = MASK_X /2;
+    int yBound = MASK_Y /2;
 
-    if (tid >= width) return;
+    if (x >= width || y >= height)
+        return;
+    __shared__ int shared_mask[MASK_N][MASK_X][MASK_Y];
+    shared_mask[(x+y)%MASK_N][(x+y)/MASK_N%MASK_X][(x+y)/MASK_N/MASK_X%MASK_Y] = mask[(x+y)%MASK_N][(x+y)/MASK_N%MASK_X][(x+y)/MASK_N/MASK_X%MASK_Y];
+    __syncthreads();
+    float val[MASK_N*3] = {0.0};
 
-    int x = tid;
-    for (int y = 0; y < height; ++y) {
-        for (int i = 0; i < Z; ++i) {
-            
-            val[i][2] = zero;
-            val[i][1] = zero;
-            val[i][0] = zero;
-
-            for (int v = -yBound; v <= yBound; ++v) {
-                for (int u = -xBound; u <= xBound; ++u) {
-                    if (bound_check(x + u, 0, width) && bound_check(y + v, 0, height)) {
-                        const __half R = __float2half((float)s[channels * (width * (y + v) + (x + u)) + 2]);
-                        const __half G = __float2half((float)s[channels * (width * (y + v) + (x + u)) + 1]);
-                        const __half B = __float2half((float)s[channels * (width * (y + v) + (x + u)) + 0]);
-                        val[i][2] = __hadd(val[i][2], __hmul(R, __float2half(mask[i][u + xBound][v + yBound])));
-                        val[i][1] = __hadd(val[i][1], __hmul(G, __float2half(mask[i][u + xBound][v + yBound])));
-                        val[i][0] = __hadd(val[i][0], __hmul(B, __float2half(mask[i][u + xBound][v + yBound])));
-                    }
-                }
+    for (i = 0; i < MASK_N; ++i) {
+        for (v = -yBound; v < yBound + adjustY; ++v) {
+            for (u = -xBound; u < xBound + adjustX; ++u) {
+                if ((x + u) >= 0 && (x + u) < width && y + v >= 0 && y + v < height) {
+                    R = s[channels * (width * (y+v) + (x+u)) + 2];
+                    G = s[channels * (width * (y+v) + (x+u)) + 1];
+                    B = s[channels * (width * (y+v) + (x+u)) + 0];
+                    val[i*3+2] += R * shared_mask[i][u + xBound][v + yBound];
+                    val[i*3+1] += G * shared_mask[i][u + xBound][v + yBound];
+                    val[i*3+0] += B * shared_mask[i][u + xBound][v + yBound];
+                }    
             }
         }
-        __half totalR = zero;
-        __half totalG = zero;
-        __half totalB = zero;
-        for (int i = 0; i < Z; ++i) {
-            totalR = __hadd(totalR, __hmul(val[i][2], val[i][2]));
-            totalG = __hadd(totalG, __hmul(val[i][1], val[i][1]));
-            totalB = __hadd(totalB, __hmul(val[i][0], val[i][0]));
-        }
-        totalR = __hdiv(hsqrt(totalR), __float2half(SCALE));
-        totalG = __hdiv(hsqrt(totalG), __float2half(SCALE));
-        totalB = __hdiv(hsqrt(totalB), __float2half(SCALE));
-        const unsigned char cR = (__half2float(totalR) > 255.f) ? 255 : __half2float(totalR);
-        const unsigned char cG = (__half2float(totalG) > 255.f) ? 255 : __half2float(totalG);
-        const unsigned char cB = (__half2float(totalB) > 255.f) ? 255 : __half2float(totalB);
-        t[channels * (width * y + x) + 2] = cR;
-        t[channels * (width * y + x) + 1] = cG;
-        t[channels * (width * y + x) + 0] = cB;
     }
+
+    float totalR = 0.0;
+    float totalG = 0.0;
+    float totalB = 0.0;
+    for (i = 0; i < MASK_N; ++i) {
+        totalR += val[i * 3 + 2] * val[i * 3 + 2];
+        totalG += val[i * 3 + 1] * val[i * 3 + 1];
+        totalB += val[i * 3 + 0] * val[i * 3 + 0];
+    }
+
+    totalR = sqrt(totalR) / SCALE;
+    totalG = sqrt(totalG) / SCALE;
+    totalB = sqrt(totalB) / SCALE;
+    const unsigned char cR = (totalR > 255.0) ? 255 : totalR;
+    const unsigned char cG = (totalG > 255.0) ? 255 : totalG;
+    const unsigned char cB = (totalB > 255.0) ? 255 : totalB;
+    t[channels * (width * y + x) + 2] = cR;
+    t[channels * (width * y + x) + 1] = cG;
+    t[channels * (width * y + x) + 0] = cB;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
+
     assert(argc == 3);
     unsigned height, width, channels;
-    unsigned char *src = NULL, *dst;
-    unsigned char *dsrc, *ddst;
+    unsigned char* host_s = NULL;
+    read_png(argv[1], &host_s, &height, &width, &channels);
+    unsigned char* host_t = (unsigned char*) malloc(height * width * channels * sizeof(unsigned char));
+    
 
-    /* read the image to src, and get height, width, channels */
-    if (read_png(argv[1], &src, &height, &width, &channels)) {
-        std::cerr << "Error in read png" << std::endl;
-        return -1;
-    }
-
-    dst = (unsigned char *)malloc(height * width * channels * sizeof(unsigned char));
-    cudaHostRegister(src, height * width * channels * sizeof(unsigned char), cudaHostRegisterDefault);
-
+    /* Hint 1 */
     // cudaMalloc(...) for device src and device dst
-    cudaMalloc(&dsrc, height * width * channels * sizeof(unsigned char));
-    cudaMalloc(&ddst, height * width * channels * sizeof(unsigned char));
+    unsigned char* device_s;
+    unsigned char* device_t;
+    cudaMalloc((void **)&device_s, height * width * channels * sizeof(unsigned char));
+    cudaMalloc((void **)&device_t, height * width * channels * sizeof(unsigned char));
+    
 
-    // cudaMemcpy(...) copy source image to device (mask matrix if necessary)
-    cudaMemcpy(dsrc, src, height * width * channels * sizeof(unsigned char), cudaMemcpyHostToDevice);
+    /* Hint 2 */
+    // cudaMemcpy(...) copy source image to device (filter matrix if necessary)
+    cudaMemcpy(device_s, host_s, height * width * channels * sizeof(unsigned char), cudaMemcpyHostToDevice);
+    // cudaMemcpyToSymbol(const_mask, mask, MASK_N * MASK_X * MASK_Y * sizeof(int));
 
-    // decide to use how many blocks and threads
-    const int num_threads = (16, 16);
-    // const int num_blocks = height / num_threads + 1;
-    const int num_blocks2 = width / num_threads + 1;
-
-    // launch cuda kernel
-    // sobel <<<num_blocks2, num_threads>>> (dsrc, ddst, height, width, channels);
-    sobel <<< num_blocks2, num_threads >>> (dsrc, ddst, height, width, channels);
+    /* Hint 3 */
+    // acclerate this function
+    dim3 dimBlock(16, 16);
+    dim3 dimGrid((width + dimBlock.x - 1) / dimBlock.x, (height + dimBlock.y - 1) / dimBlock.y);
+    std::cout << "width = " << width << ", height = " << height << std::endl;
+    std::cout << (width + dimBlock.x - 1) / dimBlock.x << " " << (height + dimBlock.y - 1) / dimBlock.y << std::endl;
+    sobel <<< dimGrid, dimBlock >>> (device_s, device_t, height, width, channels);
+    
+    /* Hint 4 */
     // cudaMemcpy(...) copy result image to host
-    cudaMemcpy(dst, ddst, height * width * channels * sizeof(unsigned char), cudaMemcpyDeviceToHost);
+    cudaMemcpy(host_t, device_t, height * width * channels * sizeof(unsigned char), cudaMemcpyDeviceToHost);
+    write_png(argv[2], host_t, height, width, channels);
 
-    write_png(argv[2], dst, height, width, channels);
-    free(src);
-    free(dst);
-    cudaFree(dsrc);
-    cudaFree(ddst);
+    cudaFree(device_s);
+    cudaFree(device_t);
+
     return 0;
 }
-
