3a22c04d19fde1f1c4dd1bbb33624306941be104
poyuwu17@gmail.com <poyuwu17@gmail.com>
2023-12-10 14:38:12 +0000

    change the format to TA format

--- hw/hw3/hw3-2/hw3-2.cu
+++ hw/hw3/hw3-2/hw3-2.cu
@@ -6,32 +6,23 @@
 #define BLOCK_SIZE 8
 
 int V, E;
+int matrix_size;
 int *adjacency_matrix;
 
-// __global__ void phase1(){
+int ceil(int a, int b) { return (a + b - 1) / b; }
 
-// }
-
-// __global__ void phase2(){
-
-// }
-
-// __global__ void phase3(){
-
-// }
-
-int main(int argc, char *argv[]) {
-
-  // process input 
-  FILE *input_file = fopen(argv[1], "r");
+void input(char* infile) {
+  FILE *input_file = fopen(infile, "rb");
   fread(&V, sizeof(int), 1, input_file);
   fread(&E, sizeof(int), 1, input_file);
+  
+  matrix_size = ((V / BLOCK_SIZE) + 1 ) * BLOCK_SIZE; // matrix size must be multiple of BLOCK_SIZE
 
-  int matrix_size = ((V / BLOCK_SIZE) + 1 ) * BLOCK_SIZE; // matrix size must be multiple of BLOCK_SIZE
+  printf("%d, %d, %d\n", V, E, matrix_size); // mygodimatomato : for checking
   adjacency_matrix = (int *)malloc(matrix_size * matrix_size * sizeof(int)); // set the matrix to 1D array for easier data transfer
 
   cudaMallocHost((void **)&adjacency_matrix, matrix_size * matrix_size * sizeof(int)); // pinned memory for faster data transfer
-
+ 
   for(int i = 0; i < matrix_size; i++) {
     for(int j = 0; j < matrix_size; j++) {
       if (i == j)
@@ -46,15 +37,52 @@ int main(int argc, char *argv[]) {
     fread(edge, sizeof(int), 3, input_file);
     adjacency_matrix[edge[0] * matrix_size + edge[1]] = edge[2];
   }
-  // finish processing input
+  fclose(input_file);
+}
+
+void output(char* outFileName){
+  FILE* outfile = fopen(outFileName, "w");
+  for (int i = 0; i < V; i++) {
+    for (int j = 0; j < V; j++) {
+      if (adjacency_matrix[i * matrix_size + j] > MY_INF)
+        adjacency_matrix[i*matrix_size + j] = MY_INF;
+      fwrite(&adjacency_matrix[i * matrix_size + j], sizeof(int), 1, outfile);
+    }
+  }
+  fclose(outfile);
+}
+
+__global__ void phase1(int* d_dist, int r){
+
+}
+
+__global__ void phase2(){
+
+}
+
+__global__ void phase3(){
+
+}
+
+
+void block_FW() {
+  int round = ceil(V, BLOCK_SIZE);
 
   
-  // mygodimatomato : for checking
-  for (int i = 0; i < V; i++) {
-    for (int j = 0; j < V; j++)
-      printf("%d ", adjacency_matrix[i * matrix_size + j]);
-    printf("\n");
+  for (int r = 0; r < round; r++) {
+    phase1<<<>>>()
+    phase2<<<>>>
+    phase3<<<>>>
   }
+}
+
+// __global__ void phase1(){
+
+// }
+
+
+  
+
 
 
   // Allocate memory for GPU
@@ -66,19 +94,20 @@ int main(int argc, char *argv[]) {
   //   phase3<<<>>>();
   // }
 
+int main(int argc, char* argv[]) {
+  input(argv[1]);
 
-  // process output
-  FILE *output_file = fopen(argv[2], "w");
+  // mygodimatomato : for checking
   for (int i = 0; i < V; i++) {
-    for (int j = 0; j < V; j++)
-      fwrite(&adjacency_matrix[i * matrix_size + j], sizeof(int), 1, output_file);
+    for (int j = 0; j < V; j++){
+      if(adjacency_matrix[i * matrix_size + j] >= MY_INF)
+        printf("INF ");
+      else 
+        printf("%3d ", adjacency_matrix[i * matrix_size + j]);
+    } printf("\n");
   }
-  // finish processing output
-  
 
-  fclose(input_file);
-  fclose(output_file);
-  free(adjacency_matrix);
-
+  block_FW();
+  output(argv[2]);
   return 0;
 }
